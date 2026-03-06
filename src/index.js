f21d4748f89c60a51d483dc8076e38b30cc6d1b2
Hector <hledezma910@gmail.com>
2026-03-06 12:39:09 -0400

    create a form for get the location and sow the results in the page

--- src/index.js
+++ src/index.js
@@ -1,5 +1,33 @@
 import "./style.css";
 import { fetchingWeatherData } from "./api.js";
 
+const form = document.querySelector("form");
+const input = document.querySelector("input");
 
-console.log(fetchingWeatherData("Pedroldivnl"));
+form.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const location = input.value;
+
+  const data =  await fetchingWeatherData(location);
+
+  refreshUI(data);
+});
+
+function refreshUI(data) {
+  const weatherDisplay = document.querySelector("#weather-display");
+  weatherDisplay.textContent = "";
+
+  const addressText = document.createElement("h2");
+  const conditionText = document.createElement("p");
+  const tempText = document.createElement("p");
+
+  addressText.classList.add("address");
+  conditionText.classList.add("condition");
+  tempText.classList.add("temp");
+
+  addressText.textContent = `${data.address}`;
+  conditionText.textContent = `Condition: ${data.conditions}`;
+  tempText.textContent = `${data.temp}°C`;
+
+  weatherDisplay.append(addressText, conditionText, tempText);
+}
