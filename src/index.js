c111dfd23e2ef9e707bca78d684f4f492586a010
Hector <hledezma910@gmail.com>
2026-03-06 16:59:19 -0400

    add loader while fetching data

--- src/index.js
+++ src/index.js
@@ -3,24 +3,40 @@ import { fetchingWeatherData } from "./api.js";
 
 const form = document.querySelector("form");
 const input = document.querySelector("input");
+const weatherDisplay = document.querySelector("#weather-display");
 
 form.addEventListener("submit", async (e) => {
   e.preventDefault();
   const location = input.value;
+  const loader = document.querySelector("#loading-spinner");
 
-  const data = await fetchingWeatherData(location);
+  console.log("Loader element:", loader); // If this says 'null', the ID is wrong!
+  console.log("Display element:", weatherDisplay);
+  loader.classList.remove("hidden");
 
-  refreshUI(data);
+  Array.from(weatherDisplay.children).forEach((child) => {
+    if (child !== loader) child.remove();
+  });
+
+  try {
+    const data = await fetchingWeatherData(location);
+    loader.classList.add("hidden");
+    if (data) {
+      refreshUI(data);
+    }
+  } catch (err) {
+    loader.classList.add("hidden");
+    console.error("Fetch failed");
+  }
 });
 
 function refreshUI(data) {
-  const weatherDisplay = document.querySelector("#weather-display");
   weatherDisplay.textContent = "";
   const iconImg = document.createElement("img");
   const addressText = document.createElement("h2");
   const conditionText = document.createElement("p");
   const tempText = document.createElement("p");
-  console.log(data.icon)
+  console.log(data.icon);
   iconImg.src = `./assets/icons/${data.icon}.svg`;
   iconImg.classList.add("weather-icon");
   iconImg.alt = data.conditions;
